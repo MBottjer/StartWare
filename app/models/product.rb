b7af813f1b53ebe3577023cb09381de7c4333846
Michael Bottjer <michaelbottjer@hotmail.com>
2014-02-21 16:50:18 +0000

    redesigned front page presentation of products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,12 @@
 class Product < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :photo, :styles => { :medium => "500x500>"}
+  has_attached_file :photo, :styles => { :medium => "500x400>"}
+
+  def self.limit_description(string)
+    string.chars.count > 60 ? string[0...60] + "..." : string
+  end
+
+
+
 end
